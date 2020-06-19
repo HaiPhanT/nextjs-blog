cfafd745c735f4b1a4e991da9fc752af494643e0
hai phan <hai.phan@urbn8.com>
2020-06-19 10:58:16 +0700

    first commit

---
+++ pages/posts/first-post.js
@@ -0,0 +1,22 @@
+import Head from "next/head";
+import Layout from "../../components/layout";
+
+//This page is no longer used after dynamic route chapter
+export default function FisrtPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>First Post</h1>
+      <h2>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged.
+      </h2>
+    </Layout>
+  );
+}
